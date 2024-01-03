97c4f7c95f66345283dc4f78394e846d11beb295
Ahmedsabry11 <ahmedsabry232345@gmail.com>
2024-01-03 14:25:37 +0200

    try fix load

--- load_datasets.py
+++ load_datasets.py
@@ -6,14 +6,18 @@ def create_folders(paths):
     for path in paths:
         if not os.path.exists(path):
             os.makedirs(path)
+            print("created successfully path: ",path)
 
 create_folders([path_chest_imagenome, path_mimic_cxr, path_mimic_cxr_jpg])
 
 # unzip the mimic-cxr-jpg dataset
-!unzip "/content/drive/MyDrive/MIMIC/p11_subset1.zip" -d $path_mimic_cxr_jpg
-!unzip "/content/drive/MyDrive/MIMIC/chest-imagenome-dataset-1.0.0.zip" -d $path_chest_imagenome
-!unzip $path_chest_imagenome+"/silver_dataset/scene_graph.zip" -d $path_chest_imagenome+"/silver_dataset/
-!unzip "/content/drive/MyDrive/MIMIC/mimic-cxr-reports.zip" -d $path_mimic_cxr
+!unzip "/content/drive/MyDrive/MIMIC/p11_subset1.zip" -d "datasets/mimic-cxr-jpg"
+print("unzipped mimic_cxr_jpg")
+!unzip "/content/drive/MyDrive/MIMIC/chest-imagenome-dataset-1.0.0.zip" -d "datasets/chest-imagenome-dataset"
+print("unzipped chest imagnome")
+!unzip "datasets/chest-imagenome-dataset/silver_dataset/scene_graph.zip" -d "datasets/chest-imagenome-dataset/silver_dataset/
+print("unzipped scene_graph")
+!unzip "/content/drive/MyDrive/MIMIC/mimic-cxr-reports.zip" -d "datasets/mimic-cxr"
 
 # in folder mimic-cxr-jpg, move subfolder directly under mimic-cxr-jpg
 
