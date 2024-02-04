75870c02910f6a617a5c944296d4087b440f482a
zeinabmoawad <92188433+zeinabmoawad@users.noreply.github.com>
2024-02-04 22:25:30 +0200

    config gpt2 medium

--- src/language_model/GPT2/config.py
+++ src/language_model/GPT2/config.py
@@ -1,10 +1,10 @@
 
 class Config:
     max_seq_len = 512
-    d_model = 512
+    d_model = 1024
     d_ff = 2048
     dropout = 0.1
-    num_heads = 8
-    num_layers = 6
+    num_heads = 16
+    num_layers = 24
     vocab_size = 50257
     ignore_index = -100
