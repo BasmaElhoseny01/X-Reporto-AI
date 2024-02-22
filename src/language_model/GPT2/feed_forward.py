0ac7b9a3aa02019ec5667f68f11ba4d4b790ab00
zeinabmoawad <92188433+zeinabmoawad@users.noreply.github.com>
2024-02-22 23:29:39 +0200

    Documentation of GPT2

--- src/language_model/GPT2/feed_forward.py
+++ src/language_model/GPT2/feed_forward.py
@@ -1,16 +1,46 @@
+from typing import Optional
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
 class FeedForward(nn.Module):
+    """
+    Feedforward neural network layer with linear transformations and dropout.
+
+    Args:
+        config (Config): An instance of the configuration class with model hyperparameters.
+    """
+
     def __init__(self, config):
+        """
+        Initializes the FeedForward module.
+
+        Parameters:
+        - config (Config): An instance of the configuration class with model hyperparameters.
+        """
         super(FeedForward, self).__init__()
         self.config = config
         self.fc1 = nn.Linear(self.config.d_ff1, self.config.d_ff2)
         self.fc2 = nn.Linear(self.config.d_ff2, self.config.d_ff3)
         self.dropout = nn.Dropout(self.config.dropout)
 
-    def forward(self, x):
+    def forward(self, x: Optional[torch.Tensor] = None):
+        """
+        Forward pass of the FeedForward module.
+
+        Parameters:
+        - x (torch.Tensor): Input tensor.
+
+        Returns:
+        - torch.Tensor: Output tensor after applying linear transformations, ReLU activation,
+          and dropout.
+        """
+        """
+        Some Explaintion:
+        feedforward neural network layer with linear transformations and dropout.
+        FFN(x) = max(0, xW1 + b1)W2 + b2 
+        """
+
         x = F.relu(self.fc1(x))
         x = self.dropout(x)
         x = self.fc2(x)
--- src/language_model/GPT2/layer_normalization.py
+++ src/language_model/GPT2/layer_normalization.py
@@ -1,10 +1,22 @@
 import torch
 import torch.nn as nn
-import torch.nn.functional as F
-import math
 
 class LayerNormalization(nn.Module):
+    """
+    Layer normalization module for normalizing the activations of a neural network layer.
+    Layer normalization ensures “all neurons in a particular layer effectively have the same distribution across all features for a given input.”
+
+    Args:
+        config (Config): An instance of the configuration class with model hyperparameters.
+    """
+
     def __init__(self, config):
+        """
+        Initializes the LayerNormalization module.
+
+        Parameters:
+        - config (Config): An instance of the configuration class with model hyperparameters.
+        """
         super(LayerNormalization, self).__init__()
         self.config = config
         self.gamma = nn.Parameter(torch.ones(self.config.d_model))
@@ -12,9 +24,23 @@ class LayerNormalization(nn.Module):
         self.eps = 1e-05
 
     def forward(self, x):
-        mean = x.mean(-1, keepdim=True) # (batch_size, max_seq_len, 1)
-        std = x.std(-1, keepdim=True) # (batch_size, max_seq_len, 1)
-        return self.gamma * (x - mean) / (std + self.eps) + self.beta # (batch_size, max_seq_len, d_model)
+        """
+        Forward pass of the LayerNormalization module.
+
+        Parameters:
+        - x (torch.Tensor): Input tensor.
+
+        Returns:
+        - torch.Tensor: Output tensor after applying layer normalization.
+          Shape: (batch_size, max_seq_len, d_model)
+        """
+        """
+        Some Explaintion:
+        """
+        mean = x.mean(-1, keepdim=True)  # (batch_size, max_seq_len, 1)
+        std = x.std(-1, keepdim=True)  # (batch_size, max_seq_len, 1)
+        return self.gamma * (x - mean) / (std + self.eps) + self.beta  # (batch_size, max_seq_len, d_model)
+
     
 if __name__ == '__main__':
     # Test
