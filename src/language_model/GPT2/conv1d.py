7443c192ba4991d2fc17a518a8b97070e9699394
zeinabmoawad <92188433+zeinabmoawad@users.noreply.github.com>
2024-03-19 00:22:24 +0200

    Revert "Documentation of GPT2"
    This reverts commit 0ac7b9a3aa02019ec5667f68f11ba4d4b790ab00.

--- src/language_model/GPT2/conv1d.py
+++ src/language_model/GPT2/conv1d.py
@@ -1,23 +1,22 @@
+from typing import Optional, Tuple
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
+import math
+
 
 class Conv1D(nn.Module):
     """
+    1D-convolutional layer as defined by Radford et al. for OpenAI GPT (and also used in GPT-2).
+
     Basically works like a linear layer but the weights are transposed.
 
     Args:
-        nf (int): The number of output features.
-        nx (int): The number of input features.
+        nf (`int`): The number of output features.
+        nx (`int`): The number of input features.
     """
 
     def __init__(self, nf, nx):
-        """
-        Initializes a 1D-convolutional layer.
-
-        Parameters:
-        - nf (int): The number of output features.
-        - nx (int): The number of input features.
-        """
         super().__init__()
         self.nf = nf
         self.weight = nn.Parameter(torch.empty(nx, nf))
@@ -25,16 +24,8 @@ class Conv1D(nn.Module):
         nn.init.normal_(self.weight, std=0.02)
 
     def forward(self, x):
-        """
-        Forward pass of the 1D-convolutional layer.
-
-        Parameters:
-        - x (torch.Tensor): Input tensor.
-
-        Returns:
-        - torch.Tensor: Output tensor after applying the convolutional layer.
-        """
         size_out = x.size()[:-1] + (self.nf,)
         x = torch.addmm(self.bias, x.view(-1, x.size(-1)), self.weight)
         x = x.view(size_out)
         return x
+
--- src/language_model/GPT2/residual_connection.py
+++ src/language_model/GPT2/residual_connection.py
@@ -1,50 +1,24 @@
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
 from src.language_model.GPT2.layer_normalization import LayerNormalization
+import math
 
 class ResidualConnection(nn.Module):
-    """
-    Residual connection module with layer normalization and dropout.
-    To facilitate the training and improve the learning capability of deep networks.
-
-    Args:
-        config (Config): An instance of the configuration class with model hyperparameters.
-    """
-
     def __init__(self, config):
-        """
-        Initializes the ResidualConnection module.
-
-        Parameters:
-        - config (Config): An instance of the configuration class with model hyperparameters.
-        """
         super(ResidualConnection, self).__init__()
         self.config = config
         self.ln = LayerNormalization(self.config)
         self.dropout = nn.Dropout(self.config.dropout)
 
-    def forward(self, x, sublayer, is_attention=False):
-        """
-        Forward pass of the ResidualConnection module.
-
-        Parameters:
-        - x (torch.Tensor): Input tensor.
-        - sublayer (nn.Module): Sublayer module applied within the residual connection.
-        - is_attention (bool, optional): Whether the sublayer is an attention layer.
-
-        Returns:
-        - torch.Tensor or Tuple: Output tensor after applying the residual connection.
-          If is_attention is True, returns a tuple (x, present, output_attentions).
-          Shape: (batch_size, max_seq_len, d_model)
-        """
+    def forward(self, x, sublayer,is_attention=False):
         if is_attention:
             outputs = sublayer(self.ln(x))
             x = x + self.dropout(outputs[0])
-            # return the same outputs as the attention layer
+            # return same outputs as the attention layer
             outputs = (x,) + outputs[1:]
-            return outputs  # tuple (x, present, output_attentions)
-        return x + self.dropout(sublayer(self.ln(x)))
-
+            return outputs # tuple (x, present, output_attentions)
+        return x + self.dropout(sublayer(self.ln(x))) # (batch_size, max_seq_len, d_model)
 
 if __name__ == '__main__':
     # Test
