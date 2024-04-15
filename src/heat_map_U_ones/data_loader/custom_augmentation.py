70c07f5626d3243915ecb3eb09191da8e76882a1
BasmaElhoseny <basmaelhoseny6@gmail.com>
2024-04-15 23:20:42 +0200

    Update custom_augmentation.py

--- src/heat_map_U_ones/data_loader/custom_augmentation.py
+++ src/heat_map_U_ones/data_loader/custom_augmentation.py
@@ -16,6 +16,8 @@ import random
 from torchvision import transforms
 from config import HEAT_MAP_IMAGE_SIZE
 
+ANGLE=2
+
 class CustomAugmentation(object):
     def __init__(self, transform_type):
             self.transform=TransformLibrary(transform_type)
@@ -31,6 +33,10 @@ class TransformLibrary(object):
             self.transform =A.Compose([
             # A.RandomCrop(height=256, width=256, p=1.0),
             # A.RandomHorizontalFlip(),
+            A.GaussNoise(),
+            #  rotate between -2 and 2 degrees
+            A.Affine(mode=cv2.BORDER_CONSTANT, cval=0, rotate=(-ANGLE, ANGLE)),
+
             A.Resize(height=HEAT_MAP_IMAGE_SIZE, width=HEAT_MAP_IMAGE_SIZE, p=1.0),
             A.Normalize(mean=(0.485, 0.456, 0.406), std=(0.229, 0.224, 0.225)),
             ToTensorV2(p=1.0),], p=1.0)
