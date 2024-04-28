250b49eb096788205a98f9b00023d472eefa1507
BasmaElhoseny <basmaelhoseny6@gmail.com>
2024-04-28 14:51:41 +0300

    Update create_heat_map_csv.py

--- scripts/create_heat_map_csv.py
+++ scripts/create_heat_map_csv.py
@@ -1,21 +1,6 @@
 import pandas as pd
 import argparse
 
-# # Read the CSV files
-# df1 = pd.read_csv('datasets/mimic-cxr-2.0.0-chexpert.csv')
-# df2 = pd.read_csv('datasets/train_full.csv')
-
-# # Merge the DataFrames
-# mergedColumn = ["subject_id","study_id"]
-# merged_df = pd.merge(df1, df2, on=mergedColumn)
-
-# # remove column number 10 from csv file
-# merged_df=merged_df.drop(merged_df.columns[10],axis=1)
-
-# # Write the merged DataFrame to a new CSV file
-# merged_df.to_csv('datasets/heat_map_full.csv', index=False)
-
-
 def merge_csv(df1, df2):
     # Merge the DataFrames
     mergedColumn = ["subject_id","study_id"]
