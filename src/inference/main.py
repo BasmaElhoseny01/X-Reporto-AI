10442aee55683f666fedfa73458c937007637b7a
BasmaElhoseny <basmaelhoseny6@gmail.com>
2024-05-04 18:46:30 +0300

    Added Inference Notebook

--- src/inference/main.py
+++ src/inference/main.py
@@ -1,11 +1,14 @@
+import argparse
 import cv2
+
 import albumentations as A
 from albumentations.pytorch import ToTensorV2
 import torch
 
+# Utility functions
 from src.utils import plot_single_image
 
-# Modules
+# Models
 from src.x_reporto.models.x_reporto_v1 import XReportoV1
 from transformers import GPT2Tokenizer
 
@@ -77,63 +80,25 @@ class Inference:
 
     #     pass
 
-
-
-
-# from src.object_detector.models.object_detector_factory import ObjectDetector
-# from src.binary_classifier.models.binary_classifier_selection_region_factory import BinaryClassifierSelectionRegion
-# from src.binary_classifier.models.binary_classifier_region_abnormal_factory import BinaryClassifierRegionAbnormal
-# from src.language_model.GPT2.gpt2_model import CustomGPT2
-# from src.language_model.GPT2.config import Config
 if __name__=="__main__":
+    # Take image path from command line
+    parser = argparse.ArgumentParser()
+    parser.add_argument('image_path', type=str, help='Path to the image file')
+
+    args = parser.parse_args()
+
+    image_path = args.image_path
+    print(image_path)
+
     
     # Initialize the Inference class
     inference = Inference()
 
     # Generate the report
-    inference.generate_image_report(image_path='./datasets/mimic-cxr-jpg/files/p10/p10003299/s57344656/f5414268-e553a141-39841839-4f303c85-d94d1190.jpg')
+    inference.generate_image_report(image_path=image_path)
 
-    
-    pass    
-       
+          
     
     
-    # For Debugging
-    # Create Object detector
-    # object_detector = ObjectDetector().create_model()
-    # torch.save(object_detector.state_dict(), "models/object_detector_best.pth")
-
-
-    #selection_region = BinaryClassifierSelectionRegion().create_model()
-    #torch.save(selection_region.state_dict(), "models/region_classifier_best.pth")
     
-    # region_abnormal=BinaryClassifierRegionAbnormal().create_model()
-    # torch.save(region_abnormal.state_dict(), "models/abnormal_classifier_best.pth")
-    
-    # lm=CustomGPT2
-    # config = Config()
-    ## load small gpt2 config
-    #config.d_model = 768
-    #config.d_ff1 = 768
-    #config.d_ff2 = 768
-    #config.d_ff3 = 768
-    #config.num_heads = 12
-    #config.num_layers = 12
-    #config.vocab_size = 50257
-    #config.pretrained_model = "gpt2"
-    #config.max_seq_len = 1024
-    #config.ignore_index = -100
-    #image_config = Config()
-    #image_config.d_model = 1024
-    #image_config.d_ff1 = 1024
-    #image_config.d_ff2 = 1024
-    ## image_config.d_ff2 = 768
-    #image_config.d_ff3 = 768
-    #image_config.num_heads = 16
-    #image_config.num_layers = 24
-    #image_config.vocab_size = 50257
-    #lm = CustomGPT2(config,image_config)
-    #torch.save(lm.state_dict(), "models/LM_best.pth")
-    
-    
-# python -m src.inference.main
+# python -m src.inference.main "./datasets/images/00000001_000.png"
